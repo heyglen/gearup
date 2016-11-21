86afc6b475a3f0233d59d352332150ddba1af52e
Glen Harmon <glencharmon@gmail.com>
2016-11-21 19:47:24 +0100

    21-11-2016 19:47:47

--- gearup/commands/operating_system.py
+++ gearup/commands/operating_system.py
@@ -2,20 +2,27 @@
 
 import sys
 import logging
+import platform
 
 
 logger = logging.getLogger(__name__)
 
 
 class OperatingSystem(object):
+    _os_name = None
 
     @classmethod
     def name(cls):
-        logger.debug('Get platform')
-        os_name = 'unknown'
-        if sys.platform == 'win32':
-            os_name = 'windows'
-        return os_name
+        logger.debug('Get OS name')
+        if cls._os_name is None:
+            os_name = 'unknown'
+            if sys.platform == 'win32':
+                os_name = 'windows'
+            else:
+                os_name = platform.linux_distribution()[0].lower()
+            cls._os_name = os_name
+        logger.debug('OS is {}'.format(cls._os_name))
+        return cls._os_name
 
     @classmethod
     def execute(cls, ctx, command, sudo=True):
